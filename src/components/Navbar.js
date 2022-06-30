d921375e1b4b3d3ac6e26d2a7629d55ead27c020
haerulmutakin <haerulm.takin@gmail.com>
2022-06-30 10:50:18 +0700

    temporary navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <div className="navbar-brand">
+                <h2>Cinta Koding</h2>
+            </div>
+            <div className="navbar-page-title">Post</div>
+            <div className="navbar-profile">Welcome, Takin</div>
+        </div>
+     );
+}
+ 
+export default Navbar;
