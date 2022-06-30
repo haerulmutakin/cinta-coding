8a1f084a975d1c348a433df2462a0c41f77ce153
haerulmutakin <haerulm.takin@gmail.com>
2022-06-30 11:00:34 +0700

    temporary langing page

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+const Landing = () => {
+    return ( 
+        <div className="langing-page">
+            <img alt="cinta-koding" src="programmer.png" />
+        </div>
+     );
+}
+ 
+export default Landing;
