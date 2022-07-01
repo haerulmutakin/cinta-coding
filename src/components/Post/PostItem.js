05b0d2d4d250c75cbf45e209636f923a1d42382b
haerulmutakin <haerulm.takin@gmail.com>
2022-07-01 15:56:43 +0700

    refactoring

--- src/components/Post/PostItem.js
+++ src/components/Post/PostItem.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
 import Api from '../../api/json-placeholder.api';
